10146c62121585b6c475e5246ed3d93b70dbe3d8
Brandon Shin <brandonjshin98@gmail.com>
2022-12-09 13:47:26 -0800

    Expense comments and groups models done.

---
+++ app/models/groups.py
@@ -0,0 +1,11 @@
+from .db import db, environment, SCHEMA
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String, nullable=False)
