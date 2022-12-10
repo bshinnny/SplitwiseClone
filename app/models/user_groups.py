87390d232e9fdf5e714ea0c6de93cfa25b2b6761
Brandon Shin <brandonjshin98@gmail.com>
2022-12-09 16:20:12 -0800

    Migrations added.

--- app/models/user_groups.py
+++ app/models/user_groups.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class UserGroup(db.Model):
     __tablename__ = 'user_groups'
@@ -6,6 +6,7 @@ class UserGroup(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('groups.id')), nullable=False)
 
