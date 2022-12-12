a1c78c10534a4b9d2b83a748cf349ceb0992d91c
Edmund Ju <edmundju@berkeley.edu>
2022-12-11 18:50:56 -0800

    add get all group expenses route

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -16,8 +16,32 @@ def get_all_group_routes(group_id):
     """
     group = Group.query.get(group_id)
 
+    if(not group):
+        return {"error": "Group not found"}, 404
+
     group_expenses = Expense.query.options(joinedload(Expense.recipient)).options(joinedload(Expense.user)).filter(Expense.group_id == group_id)
 
-    group_expenses_to_dict = [expense.to_dict() for expense in group_expenses]
+    def group_expenses_to_dict(expense):
+                return {
+            "id": expense.id,
+            "description": expense.description,
+            "user_id": expense.user_id,
+            "group_id": expense.group_id,
+            "recipient_id": expense.recipient_id,
+            "amount": expense.amount,
+            "date": expense.date,
+            "note": expense.note,
+            "status": expense.status,
+            "Fronter": {
+                "first_name": expense.user.first_name,
+                "last_name": expense.user.last_name,
+                "nickname": expense.user.nickname
+            },
+            "Recipient": {
+                "first_name": expense.recipient.first_name,
+                "last_name": expense.recipient.last_name,
+                "nickname": expense.recipient.nickname
+            }
+        }
 
-    return {'test': group_expenses_to_dict}
+    return {'Group Expenses': [group_expenses_to_dict(item) for item in group_expenses]}
