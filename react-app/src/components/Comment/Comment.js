1e597d18b7670d6374538809b01f223578bc5c33
Lijuan Xu <lijuanxu@hotmail.com>
2022-12-13 12:41:30 -0600

    comment-css

--- react-app/src/components/Comment/Comment.js
+++ react-app/src/components/Comment/Comment.js
@@ -2,19 +2,32 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getComments } from "../../store/comments";
+import './comment.css'
 
 function CommentsOfExpense () {
     const { expenseId } = useParams()
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(getComments(expenseId))
     }, [])
-    const comments = useSelector(state => state.comment)
+
+    const comments = useSelector(state => Object.values(state.comment))
+    if (!comments) return null;
+
     return (
         <div>
             <label><i className="fa-regular fa-comment"></i>NOTES AND COMMENTS</label>
             <div>
-
+                {comments.map(comment => (
+                    <div className="comment-detail">
+                        <div>
+                            <li>{comment.User?.first_name}.{comment.User?.last_name[0]}<span>{comment.date.slice(0,16)}</span></li>
+                            <li>{comment.description}</li>
+                        </div>
+                        <button>Delete</button>
+                    </div>
+                ))}
             </div>
             <textarea placeholder="Add a comment"></textarea>
         </div>
