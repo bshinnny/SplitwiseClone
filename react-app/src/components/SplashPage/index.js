861b6a9ae4e58d1789820f3f73ac14a690b85f2a
Lijuan Xu <lijuanxu@hotmail.com>
2022-12-14 23:27:51 -0600

    splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,7 +1,13 @@
 // import { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from 'react-router-dom';
 import './SplashPage.css'
 import background from '../../assets/background.png'
+import iphone1 from '../../assets/iphone/iphone1.png'
+import iphone2 from '../../assets/iphone/iphone2.png'
+import iphone3 from '../../assets/iphone/iphone3.png'
+import iphone4 from '../../assets/iphone/iphone4.png'
+import iphone5 from '../../assets/iphone/iphone5.png'
 
 
 export default function SplashPage() {
@@ -27,22 +33,52 @@ export default function SplashPage() {
                     </div>
                     <div id="white-small-text">Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.</div>
                     <button >Sign up</button>
+                    <div>Free for iPhone, Android, and web.</div>
                     </div>
                 </div>
                 <div className="white-right-container">
                     <i className="fa-solid fa-plane"></i>
                 </div>
             </div>
-            <div className="gray-and-green-container">
-                <div className="black-container"></div>
-                <div className="green-container"></div>
+            <div className="row-container">
+                <div className="block-container">
+                    <h1>Track balances</h1>
+                    <div>Keep track of shared expenses,</div>
+                    <div>balances, and who owes who.</div>
+                    <img src={iphone1}/>
+                </div>
+                <div className="block-container">
+                    <h1>Organize expenses</h1>
+                    <div>Split expenses with any group: trips,</div>
+                    <div>housemates, friends, and family.</div>
+                    <img src={iphone2}/>
+                </div>
             </div>
-            <div className="orange-and-black-container">
-                <div className="orange-container"></div>
-                <div className="black-container"></div>
+            <div className="row-container">
+                <div className="block-container">
+                    <h1>Add expenses easily</h1>
+                    <div>Quickly add expenses on the go before</div>
+                    <div>you forget who paid.</div>
+                    <img src={iphone3}/>
+                </div>
+                <div className="block-container">
+                    <h1>Pay friends back</h1>
+                    <div>Settle up with a friend and record any</div>
+                    <div>cash or online payment.</div>
+                    <img src={iphone4}/>
+                </div>
             </div>
-            <div className="purple-container">
-
+            <div className="row-container">
+                <div className="block-container">
+                    <h1>Get even more with PRO</h1>
+                    <div>Get even more organized with receipt scanning,</div>
+                    <div>charts and graphs, currency conversion, and</div>
+                    <div>more!</div>
+                    <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                        <button id='sign-up-button-2'>Sign Up</button>
+                    </NavLink>
+                </div>
+                <img src={iphone5}/>
             </div>
 
         </div>
