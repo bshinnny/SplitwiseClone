caa4b29c1a28904f83d499cf9c12f0940b772c15
vivian wang <viviannewang_dy@yahoo.com>
2022-12-15 22:09:36 -0800

    added no expense img

--- react-app/src/components/friends/FriendDetail.js
+++ react-app/src/components/friends/FriendDetail.js
@@ -9,6 +9,7 @@ import { NavLink } from "react-router-dom";
 import FriendSideBar from './SideBar';
 import userImg from '../../assets/profileImg.png'
 import "./FriendDetail.css";
+import nothingImg from "../../assets/person.png"
 
 
 
@@ -83,7 +84,7 @@ const FriendDetail = () => {
         <li className='expense-lists' key ={expense.id} id ={expense.id}><div className='description-div'><i class="fa-solid fa-receipt"></i>&nbsp;
         {"   " + expense.description}</div> <div className='owe-paid-div'><span className='payment-des'>you paid<p className='paid-dollar'>${expense.amount*2}</p></span><span className='payment-des'>{friendDetail.first_name} lent you <p className='lent-dollar'>${expense.amount}</p></span></div></li> 
         : <li className='expense-lists' id ={expense.id}><div className='description-div'><i class="fa-solid fa-receipt"></i>&nbsp;{"   "+expense.description}</div>
-        <div className='owe-paid-div'><span className='payment-des'>{friendDetail.first_name} paid <p className='paid-dollar'>${expense.amount*2}</p></span> <span className='payment-des'> you lent {friendDetail.first_name}<p className='lent-dollar'>${expense.amount}</p></span></div></li>) : <p>You don't have any expenses yet</p>}
+        <div className='owe-paid-div'><span className='payment-des'>{friendDetail.first_name} paid <p className='paid-dollar'>${expense.amount*2}</p></span> <span className='payment-des'> you lent {friendDetail.first_name}<p className='lent-dollar'>${expense.amount}</p></span></div></li>) :<div className='nothing'><img src={nothingImg}/><p className='p-nothing'>You don't have any expenses yet</p></div>}
       {/* {friendDetail && expensesList.map(expense=><li id ={expense.id}>{expense.description} {expense.amount}</li>) } */}
     </div>
             </div>
